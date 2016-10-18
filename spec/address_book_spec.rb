7a3157381a42141005ce5652ea4a4035c92e6e6d
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-10-18 14:34:15 -0700

    quick update

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe AddressBook do
     expect(entry.email).to eq expected_email
   end
 
+
+
   describe "attributes" do
     it "responds to entries" do
       expect(book).to respond_to(:entries)
@@ -95,7 +97,7 @@ RSpec.describe AddressBook do
     end
   end
 
-  describe "import_from_csv part 2" do
+  describe "import_from_csv alphabetical sort test" do
     it "imports the correct number of entries" do
       book.import_from_csv("entries_2.csv")
       book_size = book.entries.size
@@ -106,7 +108,7 @@ RSpec.describe AddressBook do
     it "imports the 1st entry" do
       book.import_from_csv("entries_2.csv")
 
-      entry_one = book.entries[0]
+      entry_one = book.entries[2]
 
       check_entry(entry_one, "Jeff", "555-555-4854", "bill@blocmail.com")
 
@@ -115,7 +117,7 @@ RSpec.describe AddressBook do
     it "imports the 2nd entry" do
       book.import_from_csv("entries_2.csv")
       # Check the second entry
-      entry_two = book.entries[1]
+      entry_two = book.entries[0]
 
       check_entry(entry_two, "Greg", "555-555-5415", "bob@blocmail.com")
 
@@ -124,7 +126,7 @@ RSpec.describe AddressBook do
     it "imports the 3rd entry" do
       book.import_from_csv("entries_2.csv")
       # Check the third entry
-      entry_three = book.entries[2]
+      entry_three = book.entries[1]
 
       check_entry(entry_three, "Holly", "555-555-3660", "joe@blocmail.com")
 

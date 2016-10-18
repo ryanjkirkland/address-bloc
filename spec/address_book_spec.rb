4ba5c35216b1645b2167f9c8986e90cdab987071
Ryan 'Thor' Kirkland <ryankirklandga@gmail.com>
2016-10-17 20:59:31 -0700

    add tests for import_from_csv - failing

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -95,4 +95,40 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "import_from_csv part 2" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq(3)
+    end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+
+      entry_one = book.entries[0]
+
+      check_entry(entry_one, "Jeff", "555-555-4854", "bill@blocmail.com")
+
+    end
+
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the second entry
+      entry_two = book.entries[1]
+
+      check_entry(entry_two, "Greg", "555-555-5415", "bob@blocmail.com")
+
+    end
+
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      # Check the third entry
+      entry_three = book.entries[2]
+
+      check_entry(entry_three, "Holly", "555-555-3660", "joe@blocmail.com")
+
+    end
+  end
+
 end
